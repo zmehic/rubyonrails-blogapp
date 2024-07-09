9a5519fae13dca7aa97a95d5995993594ff449bf
zmehic <zaim.mehic_7@hotmail.com>
2024-07-09 19:30:54 +0200

    From sqlitetoposgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_07_08_104339) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
