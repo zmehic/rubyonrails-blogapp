003684c3e2aeafebc533c7ffcbd7ff06dc8b73cd
zmehic <zaim.mehic_7@hotmail.com>
2024-07-13 16:19:10 +0200

    ActionText

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,7 @@
 class BlogPost < ApplicationRecord
+  has_rich_text :content
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
 
   scope :sorted, -> { order(Arel.sql("published_at DESC NULLS LAST")).order(updated_at: :ASC) }
   scope :draft, -> { where(published_at: nil) }
